2b6e25e569e238b96a2b4ba20e1a7fdfa99cad4e
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-09-15 17:51:23 +0200

    using default spring security auth with USER and ADMIN role

--- src/main/java/com/nadarzy/springsecurity/HomeResource.java
+++ src/main/java/com/nadarzy/springsecurity/HomeResource.java
@@ -3,14 +3,22 @@ package com.nadarzy.springsecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by Julian Nadarzy on 15/09/2021
- */
+/** Created by Julian Nadarzy on 15/09/2021 */
 @RestController
 public class HomeResource {
 
-    @GetMapping("/")
-    public String home(){
-        return ("<h1>Hello world</h1>");
-    }
+  @GetMapping("/")
+  public String home() {
+    return ("<h1>Hello world</h1>");
+  }
+
+  @GetMapping("/user")
+  public String user() {
+    return ("<h1>Hello user</h1>");
+  }
+
+  @GetMapping("/admin")
+  public String admin() {
+    return ("<h1>Hello admin</h1>");
+  }
 }
--- src/main/java/com/nadarzy/springsecurity/SecurityConfiguration.java
+++ src/main/java/com/nadarzy/springsecurity/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.nadarzy.springsecurity;
 import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,7 +23,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
         .withUser("foo")
         .password("foo")
-        .roles("USER");
+        .roles("ADMIN");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .antMatchers("/admin")
+        .hasRole("ADMIN")
+        .antMatchers("/user")
+        .hasAnyRole("USER", "ADMIN")
+        .antMatchers("/", "static/css", "static/js")
+        .permitAll()
+        .and()
+        .formLogin();
   }
 
   @Bean
