a526ac7d12df9994878cc099c05a14590d18ce87
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-09-16 17:59:11 +0200

    using h2 with custom schema to load "real" users from db

--- src/main/java/com/nadarzy/springsecurity/SecurityConfiguration.java
+++ src/main/java/com/nadarzy/springsecurity/SecurityConfiguration.java
@@ -1,14 +1,11 @@
 package com.nadarzy.springsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -23,7 +20,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-    auth.jdbcAuthentication().dataSource(dataSource);
+    auth.jdbcAuthentication()
+        .dataSource(dataSource)
+        .usersByUsernameQuery(
+            "select username, password, enabled" + " from users " + "where username = ?")
+        .authoritiesByUsernameQuery(
+            "select username, authority from authorities where username = ?");
 
     // to preload some users into db
 
